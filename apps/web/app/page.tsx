9f25d937573941a5dfd935cc6075348831b4e0bd
Prajyot Shinde <prajyotshinde60@gmail.com>
2025-12-05 15:39:35 +0530

    Enhance user greeting display in Home component

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,7 +6,9 @@ export default async function Home() {
   const user = await client.user.findFirst();
   return (
     <div >
-      {user?.username}
+      <h1>Welcome, {user?.username}</h1>
+      
+      <h2>Your password is</h2>
       {user?.password}
     </div>
   );
